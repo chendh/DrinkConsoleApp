8fdf9f5375fa238b26cc88fa619cbdac9a4da7dc
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-05-22 14:22:05 +0800

    drink console app (Ver1)

--- DrinkConsoleApp/DrinkConsoleApp.cpp
+++ DrinkConsoleApp/DrinkConsoleApp.cpp
@@ -1,20 +1,59 @@
-﻿// DrinkConsoleApp.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "DrinkItem.h"
+#include "DrinkConsoleApp.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<DrinkItem> drinks;
+
+    AddNewDrink(drinks);
+    DisplayDrinkMenu(drinks);
+    return 0;
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+void AddNewDrink(vector<DrinkItem>& drinks)
+{
+    //DrinkItem drink1("紅茶", "大杯", 60);
+    //drinks.push_back(drink1);
 
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+    drinks.push_back(DrinkItem("紅茶", "大杯", 60));
+    drinks.push_back(DrinkItem("紅茶", "中杯", 40));
+    drinks.push_back(DrinkItem("綠茶", "大杯", 60));
+    drinks.push_back(DrinkItem("綠茶", "中杯", 40));
+    drinks.push_back(DrinkItem("咖啡", "大杯", 80));
+    drinks.push_back(DrinkItem("咖啡", "中杯", 60));
+    drinks.push_back(DrinkItem("可樂", "大杯", 30));
+    drinks.push_back(DrinkItem("可類", "中杯", 20));
+}
+
+void DisplayDrinkMenu(vector<DrinkItem>& drinks) {
+    cout << setw(4) << "編號" << setw(10) << "飲料名稱" << setw(10) << "大小" << setw(10) << "價格" << endl;
+    cout << "----------------------------------" << endl;
+
+    int count = 1;
+    //for (int i = 0; i < drinks.size(); i++) {
+    //    cout << setw(4) << count;
+    //    drinks[i].displayItem();
+    //    count++;
+    //}
+
+    //for (DrinkItem item : drinks) {
+    //    cout << setw(4) << count;
+    //    item.displayItem();
+    //    count++;
+    //}
+
+    vector<DrinkItem>::iterator v = drinks.begin();
+    while (v != drinks.end()) {
+        cout << setw(4) << count;
+        v->displayItem();
+        v++;
+        count++;
+    }
+
+
+    cout << "----------------------------------" << endl;
+    cout << "總共有" << drinks.size() << "項飲品" << endl;
+}
---
+++ DrinkConsoleApp/DrinkConsoleApp.h
@@ -0,0 +1,6 @@
+#pragma once
+using namespace std;
+
+void AddNewDrink(vector<DrinkItem>&);
+
+void DisplayDrinkMenu(vector<DrinkItem>&);
